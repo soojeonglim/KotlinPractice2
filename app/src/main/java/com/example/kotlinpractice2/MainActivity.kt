ba067041e7b7aa5f30f5eebe9d703d2dbc9f21b7
soojeonglim <lsj69311@gmail.com>
2022-01-27 16:34:15 +0900

    TextView

--- app/src/main/java/com/example/kotlinpractice2/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpractice2/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.kotlinpractice2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {    // 앱이 최소 실행되었을때 실행
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main)  // xml 화면 뷰를 연결
+
+        tv_title.setText("Hello World..!")  // .setText : 텍스트의 값을 변경해줌
+
     }
 }
