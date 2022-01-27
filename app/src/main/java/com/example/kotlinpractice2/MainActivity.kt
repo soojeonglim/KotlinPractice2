a080126c7643e389937a6cdc5151d3ca1685f49d
soojeonglim <lsj69311@gmail.com>
2022-01-27 17:05:47 +0900

    Intent 화면이동 및 데이터 전달

--- app/src/main/java/com/example/kotlinpractice2/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpractice2/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.kotlinpractice2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,11 +11,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)  // xml 화면 뷰를 연결
 
+        // TextView
         tv_title.setText("Hello World..!")  // .setText : 텍스트의 값을 변경해줌
 
+        // EditText&Button
         btn_getText.setOnClickListener {    // et_id에 입력되어있는 값을 가지고와서 텍스트뷰에 뿌림
             var resultText = et_id.text.toString()  // et_id에 입력되어있는값을 string 형태로 가져옴
             tv_result.setText(resultText)   // 입력된 값을 tv_result에 set 해줌
+        }
+
+        // Intent 화면이동 및 데이터 전달
+        btn_a.setOnClickListener {
+            // var : 변수, 값이 언제든기 변경 될 수 있음
+            // val : 자바에서는 final, 값이 변경되지 목하는 변수
+
+            val intent = Intent(this, SubActivity::class.java)  // 다음 화면으로 이동하기 위한 인텐트 객체 생성
+            intent.putExtra("msg",tv_sendMsg.text.toString())   // Hello World 라는 텍스트 값을 담은 뒤 msg라는 키로 잠굼
+            startActivity(intent)   // intent에 저장되어있는 액티비티 쪽으로 이동한다.
+            //finish()    // 자기 자신 액티비티를 파괴한다.
         }
     }
 }
--- app/src/main/java/com/example/kotlinpractice2/SubActivity.kt
+++ app/src/main/java/com/example/kotlinpractice2/SubActivity.kt
@@ -2,10 +2,16 @@ package com.example.kotlinpractice2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sub.*
 
 class SubActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sub)
+
+        if(intent.hasExtra("msg")) {    // .hasExtra : intent안에 있던 객체에서 "msg"라는 객체를 찾아라 그리고 있으면 to do를 실행
+            tv_getMsg.text = intent.getStringExtra("msg")    // SubActivity의 존재하는 tv_getMsg에 Hello World가 넘겨져 옴
+            // MainActivity에서 intent.putExtra("msg",tv_sendMsg.text.toString()) 해줬으므로 getExtra()가 아닌 getStringExtra()로 해줌
+        }
     }
 }
