e20082c28da202fcd2a1d0d134e8f8fb9219a25e
soojeonglim <lsj69311@gmail.com>
2022-01-27 16:40:30 +0900

    EditText&Button

--- app/src/main/java/com/example/kotlinpractice2/MainActivity.kt
+++ app/src/main/java/com/example/kotlinpractice2/MainActivity.kt
@@ -12,5 +12,9 @@ class MainActivity : AppCompatActivity() {
 
         tv_title.setText("Hello World..!")  // .setText : 텍스트의 값을 변경해줌
 
+        btn_getText.setOnClickListener {    // et_id에 입력되어있는 값을 가지고와서 텍스트뷰에 뿌림
+            var resultText = et_id.text.toString()  // et_id에 입력되어있는값을 string 형태로 가져옴
+            tv_result.setText(resultText)   // 입력된 값을 tv_result에 set 해줌
+        }
     }
 }
